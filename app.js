b5b64895a0bffef064939839afea430cb11cb179
vinod <vinodkumar.r@celestialsys.com>
2023-01-23 10:17:39 +0530

    update in multiComments

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const number = document.getElementById('numb3');
 const currentLuction = document.getElementById('getlocal');
 
 const apikey = '6f02a6599be142fa02a402b1df294756';
-/* request will fecth the data from open weather and display the content in the ui */
+/**
+ * request will fecth the data from open weather and display the content in the ui
+ * @param {name} city=has an coundry code
+ */
 const request = async (city) => {
   const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
   const mydata = await fetch(api).then((response) => response.json()).then((data) => data);
@@ -27,9 +30,10 @@ inputTag.addEventListener('keyup', (e) => {
     request(e.target.value);
   }
 });
-
-/* This will fecth the data with longitude latitude
-and display the current location data in the ui */
+/**
+ * This will fecth the data with longitude latitude
+ *  and display the current location data in the ui
+*/
 currentLuction.addEventListener('click', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async (location) => {
